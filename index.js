7668dc926198169a5fea583b329db6ddda3203a5
Tharanga Anuradha <tharanga123anuradha@gmail.com>
2024-08-14 20:07:08 +0530

    finished durm kit project

---
+++ index.js
@@ -0,0 +1,56 @@
+var buttons = document.querySelectorAll("button.drum");
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function () {
+    var buttonText = this.textContent;
+    selectAudio(buttonText);
+    buttonAnimation(buttonText);
+  });
+}
+document.addEventListener("keypress", function (event) {
+  selectAudio(event.key);
+  buttonAnimation(event.key);
+});
+// var audio = new Audio("./sounds/tom-1.mp3");
+//     audio.play();
+
+function selectAudio(key) {
+  switch (key) {
+    case "w":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "a":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "s":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "d":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "j":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "k":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "l":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+  }
+}
+
+function buttonAnimation(key) {
+  var activeButton = document.querySelector("." + key);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
